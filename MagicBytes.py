8804a3f7f36679cc1bb88eae01f65f715112da7e
root <root@localhost.localdomain>
2020-10-31 03:32:34 +0100

    Release Version

--- MagicBytes.py
+++ MagicBytes.py
@@ -3,23 +3,34 @@
 
 # This is an easy script that helps you adding the numbers that
 # identify the files.
-# Now jpg and png are available
+# Now jpg, png and gif are available
 # More extensions will be added if requested
 
 
 #IMPORTANT
 # running this will remove all the data which was stored on the file
 
-numbers = {'jpg' : [255, 216, 255, 224], 'png' : [137,80,78,71]}
+import sys
 
-filename = input("Name of the file: ")
+if len(sys.argv) == 1:
+    print("How to use: \n->python3 MagicBytes.py <yourfile> <wantedextension>")
+    print("->python3 MagicBytes.py help")
+elif len(sys.argv) == 2 and sys.argv[1] == "help":
+    print("How to use: \npython3 MagicBytes.py <yourfile> <wantedextension>")
+    print("Only jpg, png and gif are available")
+elif len(sys.argv) == 3:
+    numbers = {'jpg' : [255, 216, 255, 224, 0, 16, 74, 70, 73, 70, 0, 1], 'png' : [137,80,78,71], 'gif' : [71, 73, 70, 56, 55, 97]}
 
-try:
-    file = open(filename, 'wb')
-    fileExtension = input("Extension: ")
-    byteNumbers = bytearray(numbers[fileExtension])
+    filename = input("Name of the file: ")
 
-    file.write(byteNumbers)
-    file.close()
-except:
-    print("Error. Not such file")
+    try:
+        file = open(filename, 'wb')
+        fileExtension = input("Extension: ")
+        byteNumbers = bytearray(numbers[fileExtension])
+
+        file.write(byteNumbers)
+        file.close()
+    except:
+        print("Error. Not such file")
+else:
+    print("ERROR. Wrong number of arguments\n\n")
